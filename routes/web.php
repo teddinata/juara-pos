6a26a49dbf0a9460ed331354fb53df8d3b356bfd
teddinata <teddinata@users.noreply.github.com>
2022-09-01 23:54:58 +0700

    feat: add transaction done

--- routes/web.php
+++ routes/web.php
@@ -49,6 +49,29 @@ Route::group(['prefix' => 'apps'], function () {
         // route customers
         Route::resource('/customers', \App\Http\Controllers\Apps\CustomerController::class, ['as' => 'apps'])
             ->middleware('permission:customers.index|customers.create|customers.edit|customers.delete');
+
+        // route transactions
+        Route::get('/transactions', [\App\Http\Controllers\Apps\TransactionController::class, 'index'])->name('apps.transactions.index');
+
+        //route transaction searchProduct
+        Route::post('/transactions/searchProduct', [\App\Http\Controllers\Apps\TransactionController::class, 'searchProduct'])->name('apps.transactions.searchProduct');
+
+        //route transaction addToCart
+        Route::post('/transactions/addToCart', [\App\Http\Controllers\Apps\TransactionController::class, 'addToCart'])->name('apps.transactions.addToCart');
+
+        //route transaction destroyCart
+        Route::post('/transactions/destroyCart', [\App\Http\Controllers\Apps\TransactionController::class, 'destroyCart'])->name('apps.transactions.destroyCart');
+
+        //route transaction store
+        Route::post('/transactions/store', [\App\Http\Controllers\Apps\TransactionController::class, 'store'])->name('apps.transactions.store');
+
+        //route transaction print
+        Route::get('/transactions/print', [\App\Http\Controllers\Apps\TransactionController::class, 'print'])->name('apps.transactions.print');
+        //route sales index
+        Route::get('/sales', [\App\Http\Controllers\Apps\SaleController::class, 'index'])->middleware('permission:sales.index')->name('apps.sales.index');
+
+        //route sales filter
+        Route::get('/sales/filter', [\App\Http\Controllers\Apps\SaleController::class, 'filter'])->name('apps.sales.filter');
     });
 });
 
